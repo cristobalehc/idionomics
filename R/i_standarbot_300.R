ca4ac068fd452078f78f77a4cdf08893ab5af849
Cristóbal Hernández <cristobal.ehc@gmail.com>
2024-05-28 16:40:26 -0400

    added i_standarbot feature not to append new columns, detailed description file, and fixed license conflict

--- R/i_standarbot_300.R
+++ R/i_standarbot_300.R
@@ -7,10 +7,11 @@
 #' @param cols A vector of strings with the variable names you would like to standardize.
 #' @param idvar Your ID variable. It currently accepts only one.
 #' @param explanation Boolean to print explanation of the function (default is TRUE).
+#' @param append Boolean to append new standardized columns to your original dataframe or not (default is TRUE)
 #'
 #' @returns Will return a dataframe with new person-mean standardized columns appended at the end.
 
-i_standarbot_300 <- function (df, cols, idvar, explanation = TRUE) {
+i_standarbot_300 <- function (df, cols, idvar, explanation = TRUE, append = TRUE) {
 
   # df = a dataframe.
   # cols = a vector of strings, with column names.
@@ -31,23 +32,41 @@ i_standarbot_300 <- function (df, cols, idvar, explanation = TRUE) {
     Sys.sleep(0.4)
     cat('.',"\n")
     Sys.sleep(0.8)
-    cat('If all values for a feature within ID are NA, will return NA',"\n")
+    cat('   If all values for a feature within ID are NA, will return NA',"\n")
     Sys.sleep(0.4)
-    cat('If there is less than two values, will return 0 (zero variance)',"\n")
+    cat('   If there is less than two values, will return 0 (zero variance)',"\n")
     Sys.sleep(0.4)
-    cat('If values are constant, will return 0 (zero variance)',"\n")
+    cat('   If values are constant, will return 0 (zero variance)',"\n")
     Sys.sleep(0.4)
-    cat('If there is enough variance, will return deviations from the mean',"\n")
+    cat('   If there is enough variance, will return deviations from the mean',"\n")
   }
+
   #Apply standardization.
   df <- df %>%
     dplyr::group_by(!!rlang::sym(idvar)) %>% #We group by id.
     dplyr::mutate(dplyr::across(dplyr::all_of(cols),  #Mutate across all columns.
                   ~ if (all(is.na(.))) NA_real_ else if (sum(!is.na(.)) <2) 0 else if (sd(., na.rm=TRUE) == 0) 0 else (.-mean(., na.rm=TRUE)) / sd(., na.rm=TRUE), #Apply standardization, catch NA, one value,  and zero variance.
                   .names = "{.col}_PSD")) %>% # Add sufix.
-    dplyr::ungroup()
+    dplyr::ungroup() %>%
+    dplyr::filter()
 
-  return(df)
+  if (append == TRUE) {
+
+  cat("\n")
+    cat('   Standardization finished: Your original dataframe with standardized columns appended at the end was returned')
+  return(df) }
+
+  else {
+    #Paste column names with _PSD
+    standardized_cols = paste0(cols, "_PSD")
+
+    #Filter those columns only.
+    df <- df %>%
+      dplyr::select(!!rlang::sym(idvar), dplyr::all_of(standardized_cols))
+
+    cat('   Standardization finished: A dataframe only with your ID and standardized columns was returned')
+    return(df)
+  }
 
 }
 
--- README.md
+++ README.md
@@ -6,8 +6,10 @@ This is the first version of the "idionomics" package. Currently, it has the fol
 
 * imputatron_2000: Impute data via copy-means.
 * i_standarbot_300: Person-mean center your time-series.
+  * 28-05-2024: Added option not to append standardized columns to dataframe. 
 * IARIMAXoid_Pro: Run the I-ARIMAX algorithm.
   * 21-05-2024: Added argument to perform a comparison with an HLM growth curve model with linear time. 
+  * 24-05-2024: Added error handling routine for ARIMAX and HLM models. 
 
 #### Usage:
 
