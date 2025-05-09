b3a83703e885ba909a0c7823fd23d2a3804f5efc
Cristóbal Hernández <cristobal.ehc@gmail.com>
2025-05-09 12:59:31 -0400

    fixed plotomatron hardcoded name issue

--- R/Plotomatron.R
+++ R/Plotomatron.R
@@ -3,7 +3,7 @@
 #' @export
 #' @importFrom utils globalVariables
 #'
-#' @param dataframe Your dataframe.
+#' @param iarimax_object Your iarimax object.
 #' @param id_var Your id variable.
 #' @param y_series_name Optional: A string containing the substantive name of your y series to be used in the plot title.
 #' @param x_series_name Optional: A string containing the substantive name of your x series to be used in the plot title.
@@ -11,7 +11,7 @@
 #' @param plot_type Plot type. Supports 'caterpillar','ecdf', and 'density'. Defaults to 'caterpillar'.
 #' @param lims a vector of length 2 with limits for the density plot. Defaults to c(-1,1)
 #'
-#' @returns A list containing a dataframe with the ARIMA parameteres, plus the xreg parameter (the beta value for your x_series) together with their std.errors. If metaanalysis = TRUE, will also output a random effects meta analysis. If hlm_compare = TRUE, will also output a model comparison with HLM.
+#' @returns The plots object.
 
 
 #######################################
@@ -20,7 +20,7 @@
 
 
 
-Plotomatron <- function(dataframe,id_var ,y_series_name = NULL, x_series_name = NULL, alpha_crit_t = 0.05, plot_type = 'caterpillar', lims = c(-1,1)) {
+Plotomatron <- function(iarimax_object,id_var ,y_series_name = NULL, x_series_name = NULL, alpha_crit_t = 0.05, plot_type = 'caterpillar', lims = c(-1,1)) {
 
 
 
@@ -31,8 +31,8 @@ Plotomatron <- function(dataframe,id_var ,y_series_name = NULL, x_series_name = 
 
 
   # Check if df is an iarimax object
-  if (is.list(dataframe) && all(c('results_df', 'meta_analysis') %in% names(dataframe))) {
-    df_plt <- dataframe$results_df
+  if (is.list(iarimax_object) && all(c('results_df', 'meta_analysis') %in% names(iarimax_object))) {
+    df_plt <- iarimax_object$results_df
   } else {
     stop('Plotomatron requieres an iarimaxoid object that contains "results_df" and "meta_analysis". Run IARIMAXoid_Pro with metaanalysis = TRUE')
   }
@@ -54,7 +54,7 @@ Plotomatron <- function(dataframe,id_var ,y_series_name = NULL, x_series_name = 
 
   ### PLOTS FOR CONTEMPORANEOUS RELATIONSHIPS.
 
-  if(dataframe$type == 'contemporaneous_predictor'){
+  if(iarimax_object$type == 'contemporaneous_predictor'){
 
   #Create Caterpillar Plot.
   if (plot_type == 'caterpillar') {
@@ -75,15 +75,15 @@ Plotomatron <- function(dataframe,id_var ,y_series_name = NULL, x_series_name = 
       ggplot2::geom_point() +  # Plot the mean intercepts as points
       ggplot2::theme(plot.title = ggplot2::element_text(hjust = 0.5)) +
       ggplot2::geom_linerange(ggplot2::aes(ymin = xreg - crit_val * stderr_xreg, ymax = xreg + crit_val * stderr_xreg, color = line_color)) +  # Add error bars for +/- 2SD with color based on the new variable
-      ggplot2::scale_color_identity() + # Use the colors specified in the dataframe
+      ggplot2::scale_color_identity() + # Use the colors specified in the iarimax_object
       ggplot2::geom_hline(yintercept = 0, linetype = "dashed") +  # Add a horizontal line at y = 0 for reference
       ggplot2::coord_flip() +  # Flip the coordinates to make it a horizontal plot
       ggplot2::labs(x = "Participant ID",
                     y = "i-ARIMAX Effect Sizes and 95% Confidence Intervals of \nWithin-Person Associations",
                     title = ifelse(is.null(y_series_name) | is.null(x_series_name), 'X series Linked to Y series', paste0(x_series_name, " Linked to  \n", y_series_name)),
                     caption = "Note: Blue vertical lines represent the RE-MA pooled (nomothetic) effect in the middle\n and the lower and upper bounds of the 95% CI of the pooled effect.\nGreen horizontal lines indicate 95% CI of positive associations and red indicate negative.") +
-      ggplot2::geom_hline(yintercept = dataframe$meta_analysis$beta, color = "blue", linewidth = 1) +  # Add population-level mean intercept line
-      ggplot2::annotate("rect", xmin = -Inf, xmax = Inf, ymin = dataframe$meta_analysis$ci.lb, ymax = dataframe$meta_analysis$ci.ub, alpha = 0.2, color = "blue") + # Add 95% credible interval
+      ggplot2::geom_hline(yintercept = iarimax_object$meta_analysis$beta, color = "blue", linewidth = 1) +  # Add population-level mean intercept line
+      ggplot2::annotate("rect", xmin = -Inf, xmax = Inf, ymin = iarimax_object$meta_analysis$ci.lb, ymax = iarimax_object$meta_analysis$ci.ub, alpha = 0.2, color = "blue") + # Add 95% credible interval
       ggplot2::theme(plot.caption = ggplot2::element_text(hjust = 0)) +
       ggplot2::theme(axis.text.y = ggplot2::element_blank())
 
@@ -94,19 +94,17 @@ Plotomatron <- function(dataframe,id_var ,y_series_name = NULL, x_series_name = 
   #Plot empirical cumulative distribution function.
   if (plot_type == "ecdf") {
 
-    if('rand_df' %in% names(dataframe)) {
+    if('rand_df' %in% names(iarimax_object)) {
       #Merge random effects to I-arimax dataframe.
-      df_plt <- merge(df_plt, subset(dataframe$rand_df, select = c('Name','random_slope')), by = 'Name', all.x=TRUE)
+      df_plt <- merge(df_plt, subset(iarimax_object$rand_df, select = c(id_var,'random_slope')), by = id_var, all.x=TRUE)
     }
     else {
       stop('Random effects are requiered for ecdf plot. Please run IARIMAXoid_Pro with hlm_compare = TRUE.')
       }
 
-    #Merge MLM estimates to I-arimax dataset.
-
     #Pivot dataset to long format.
     df_plt_long <- df_plt %>%
-      dplyr::select(c("Name","xreg","random_slope","raw_correlation")) %>%
+      dplyr::select(c(!!id_var_sym,"xreg","random_slope","raw_correlation")) %>%
       dplyr::rename(Iarimax = xreg, MLM = random_slope, RawCorr = raw_correlation) %>%
       tidyr::pivot_longer(
         cols = c("Iarimax","MLM","RawCorr"),
@@ -130,9 +128,9 @@ Plotomatron <- function(dataframe,id_var ,y_series_name = NULL, x_series_name = 
 
   if (plot_type == "density") {
 
-    if('rand_df' %in% names(dataframe)) {
+    if('rand_df' %in% names(iarimax_object)) {
       #Merge random effects to I-arimax dataframe.
-      df_plt <- merge(df_plt, subset(dataframe$rand_df, select = c('Name','random_slope')), by = 'Name', all.x=TRUE)
+      df_plt <- merge(df_plt, subset(iarimax_object$rand_df, select = c(id_var,'random_slope')), by = id_var, all.x=TRUE)
     }
     else {
       stop('Random effects are requiered for density plot. Please run IARIMAXoid_Pro with hlm_compare = TRUE.')
@@ -140,7 +138,7 @@ Plotomatron <- function(dataframe,id_var ,y_series_name = NULL, x_series_name = 
 
     #Pivot dataset to long format.
     df_plt_long <- df_plt %>%
-      dplyr::select(c("Name","xreg","random_slope","raw_correlation")) %>%
+      dplyr::select(c(!!id_var_sym,"xreg","random_slope","raw_correlation")) %>%
       dplyr::rename(Iarimax = xreg, MLM = random_slope, RawCorr = raw_correlation) %>%
       tidyr::pivot_longer(
         cols = c("Iarimax","MLM","RawCorr"),
@@ -169,7 +167,7 @@ Plotomatron <- function(dataframe,id_var ,y_series_name = NULL, x_series_name = 
 
   ### PLOTS FOR LAGGED RELATIONSHIPS.
 
-  if(dataframe$type == 'lagged_predictor'){
+  if(iarimax_object$type == 'lagged_predictor'){
 
     #Create Caterpillar Plot.
     if (plot_type == 'caterpillar') {
@@ -190,15 +188,15 @@ Plotomatron <- function(dataframe,id_var ,y_series_name = NULL, x_series_name = 
         ggplot2::geom_point() +  # Plot the mean values as points
         ggplot2::theme(plot.title = ggplot2::element_text(hjust = 0.5)) +
         ggplot2::geom_linerange(ggplot2::aes(ymin = xreg_lag - crit_val * stderr_xreg_lag, ymax = xreg_lag + crit_val * stderr_xreg_lag, color = line_color)) +  # Add error bars for +/- 2SD with color based on the new variable
-        ggplot2::scale_color_identity() + # Use the colors specified in the dataframe
+        ggplot2::scale_color_identity() + # Use the colors specified in the iarimax_object
         ggplot2::geom_hline(yintercept = 0, linetype = "dashed") +  # Add a horizontal line at y = 0 for reference
         ggplot2::coord_flip() +  # Flip the coordinates to make it a horizontal plot
         ggplot2::labs(x = "Participant ID",
                       y = "i-ARIMAX Effect Sizes and 95% Confidence Intervals of \nWithin-Person Associations",
                       title = ifelse(is.null(y_series_name) | is.null(x_series_name), 'X series (t-1) Linked to Y series', paste0(x_series_name, " Linked to  \n", y_series_name)),
                       caption = "Note: Blue vertical lines represent the RE-MA pooled (nomothetic) effect in the middle\n and the lower and upper bounds of the 95% CI of the pooled effect.\nGreen horizontal lines indicate 95% CI of positive associations and red indicate negative.") +
-        ggplot2::geom_hline(yintercept = dataframe$meta_analysis$beta, color = "blue", linewidth = 1) +  # Add population-level mean intercept line
-        ggplot2::annotate("rect", xmin = -Inf, xmax = Inf, ymin = dataframe$meta_analysis$ci.lb, ymax = dataframe$meta_analysis$ci.ub, alpha = 0.2, color = "blue") + # Add 95% credible interval
+        ggplot2::geom_hline(yintercept = iarimax_object$meta_analysis$beta, color = "blue", linewidth = 1) +  # Add population-level mean intercept line
+        ggplot2::annotate("rect", xmin = -Inf, xmax = Inf, ymin = iarimax_object$meta_analysis$ci.lb, ymax = iarimax_object$meta_analysis$ci.ub, alpha = 0.2, color = "blue") + # Add 95% credible interval
         ggplot2::theme(plot.caption = ggplot2::element_text(hjust = 0)) +
         ggplot2::theme(axis.text.y = ggplot2::element_blank())
 
@@ -209,19 +207,18 @@ Plotomatron <- function(dataframe,id_var ,y_series_name = NULL, x_series_name = 
     #Plot empirical cumulative distribution function.
     if (plot_type == "ecdf") {
 
-      if('rand_df' %in% names(dataframe)) {
+      if('rand_df' %in% names(iarimax_object)) {
         #Merge random effects to I-arimax dataframe.
-        df_plt <- merge(df_plt, subset(dataframe$rand_df, select = c('Name','random_slope_lag')), by = 'Name', all.x=TRUE)
+        df_plt <- merge(df_plt, subset(iarimax_object$rand_df, select = c(id_var,'random_slope_lag')), by = id_var, all.x=TRUE)
       }
       else {
         stop('Random effects are requiered for ecdf plot. Please run IARIMAXoid_Pro_Lag with hlm_compare = TRUE.')
       }
 
-      #Merge MLM estimates to I-arimax dataset.
 
       #Pivot dataset to long format.
       df_plt_long <- df_plt %>%
-        dplyr::select(c("Name","xreg_lag","random_slope_lag","raw_correlation")) %>%
+        dplyr::select(c(!!id_var_sym,"xreg_lag","random_slope_lag","raw_correlation")) %>%
         dplyr::rename(IarimaxLag = xreg_lag, MLMLag = random_slope_lag, RawCorrLag = raw_correlation) %>%
         tidyr::pivot_longer(
           cols = c("IarimaxLag","MLMLag","RawCorrLag"),
@@ -245,9 +242,9 @@ Plotomatron <- function(dataframe,id_var ,y_series_name = NULL, x_series_name = 
 
     if (plot_type == "density") {
 
-      if('rand_df' %in% names(dataframe)) {
+      if('rand_df' %in% names(iarimax_object)) {
         #Merge random effects to I-arimax dataframe.
-        df_plt <- merge(df_plt, subset(dataframe$rand_df, select = c('Name','random_slope_lag')), by = 'Name', all.x=TRUE)
+        df_plt <- merge(df_plt, subset(iarimax_object$rand_df, select = c(id_var,'random_slope_lag')), by = id_var, all.x=TRUE)
       }
       else {
         stop('Random effects are requiered for density plot. Please run IARIMAXoid_Pro_Lag with hlm_compare = TRUE.')
@@ -255,7 +252,7 @@ Plotomatron <- function(dataframe,id_var ,y_series_name = NULL, x_series_name = 
 
       #Pivot dataset to long format.
       df_plt_long <- df_plt %>%
-        dplyr::select(c("Name","xreg_lag","random_slope_lag","raw_correlation")) %>%
+        dplyr::select(c(!!id_var_sym,"xreg_lag","random_slope_lag","raw_correlation")) %>%
         dplyr::rename(IarimaxLag = xreg_lag, MLMLag = random_slope_lag, RawCorrLag = raw_correlation) %>%
         tidyr::pivot_longer(
           cols = c("IarimaxLag","MLMLag","RawCorrLag"),
