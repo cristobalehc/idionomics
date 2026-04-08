f9138ecc249bc7e7ae4cffc1b364b2a2ec6ff16e
Cristóbal <cristobal.ehc@gmail.com>
2026-04-08 16:44:37 -0400

    review final release

--- R/idionomics-package.R
+++ R/idionomics-package.R
@@ -2,4 +2,16 @@
 #' Hernandez, C., Sahdra, B. K., Ciarrochi, J., & Hayes, S. C. (2024).
 #' Idionomic Assessment of Mindfulness. *Handbook of Assessment in Mindfulness
 #' Research*, 1--27. \url{https://doi.org/10.1007/978-3-030-77644-2_136-1}
+#'
+#' Ciarrochi, J., Sahdra, B., Hayes, S. C., Hofmann, S. G., Sanford, B.,
+#' Stanton, C., Yap, K., Fraser, M. I., Gates, K., & Gloster, A. T. (2024).
+#' A Personalised Approach to Identifying Important Determinants of Well-being.
+#' *Cognitive Therapy and Research*, *48*(4), 1--22.
+#' \url{https://doi.org/10.1007/s10608-024-10486-w}
+#'
+#' Sahdra, B. K., Ciarrochi, J., Klimczak, K. S., Krafft, J., Hayes, S. C.,
+#' & Levin, M. (2024). Testing the applicability of idionomic statistics in
+#' longitudinal studies: The example of 'doing what matters.'
+#' *Journal of Contextual Behavioral Science*, *32*, 100728.
+#' \url{https://doi.org/10.1016/j.jcbs.2024.100728}
 "_PACKAGE"
